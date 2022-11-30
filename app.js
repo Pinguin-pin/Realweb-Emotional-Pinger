47b86462cab11a435af077a439036bf51c6fb19d
Pingi <antonvdovenko03@gmail.com>
2022-11-30 19:39:13 +0200

    app.js

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
+
 let tg = window.Telegram.WebApp;
 
 tg.expand();
 
+var List_of_answers = [];
 var emotinal = ''
 
 
-for (var i = 0; i < document.getElementsByClassName('test').length; i++) {
-    
+
+
+function Emo_func(emo) {
+    emotinal = emo
+    console.log(emotinal)
+    fill(emotinal)
+  }
+
+
+
+
+function fill(str) {
+   List_of_answers.push(str)
+   console.log(List_of_answers)
 }
+
